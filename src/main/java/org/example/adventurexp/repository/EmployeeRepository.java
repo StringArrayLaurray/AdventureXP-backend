8364b6490d00f4313af6ea840bfa4428646ae581
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2025-03-04 21:16:05 +0100

    Update EmployeeRepository.java

--- src/main/java/org/example/adventurexp/repository/EmployeeRepository.java
+++ src/main/java/org/example/adventurexp/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
-package com.example.adventurexp.repository;
+package org.example.adventurexp.repository;
 
-import com.example.adventurexp.model.Employee;
+import org.example.adventurexp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
