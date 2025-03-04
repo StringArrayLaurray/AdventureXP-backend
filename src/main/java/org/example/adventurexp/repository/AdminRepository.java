a30fb6c578dffdbd5ccd47c77d061a932d8e9661
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2025-03-04 21:15:49 +0100

    Update AdminRepository.java

--- src/main/java/org/example/adventurexp/repository/AdminRepository.java
+++ src/main/java/org/example/adventurexp/repository/AdminRepository.java
@@ -1,6 +1,6 @@
-package com.example.adventurexp.repository;
+package org.example.adventurexp.repository;
 
-import com.example.adventurexp.model.Admin;
+import org.example.adventurexp.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
