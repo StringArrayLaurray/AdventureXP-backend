5016f7634e0061af55fbb9a6c858a4a89aba05b6
Celine Lund Månsson <166127204+Celinelundm@users.noreply.github.com>
2025-03-04 20:34:43 +0100

    Create EmployeeController.java
    created controller class for employee with a list all function

---
+++ src/main/java/org/example/adventurexp/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.example.adventurexpbackend.controller;
+
+import com.example.adventurexpbackend.model.Employee;
+import com.example.adventurexpbackend.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
